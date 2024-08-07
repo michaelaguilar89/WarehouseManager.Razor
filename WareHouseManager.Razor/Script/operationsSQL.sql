eb7a0b99edf897cb5abfad653f9858e879dcbcbd
MichaelAguilar89 <mchlaguilar051@hotmail.es>
2024-08-07 15:10:49 -0600

    update seetings.json

---
+++ WareHouseManager.Razor/Script/operationsSQL.sql
@@ -0,0 +1,10 @@
+SELECT "Id","Email" FROM public."AspNetUsers" 
+where "Email"="user2@email.com"
+
+SELECT * FROM public."AspNetUsers" 
+where "PhoneNumber"='8899-5241';
+
+delete from public."AspNetUsers"
+where "Email"='user2@email.com'
+
+SELECT * FROM public."AspNetUsers" 
--- WareHouseManager.Razor/obj/Release/net8.0/WareHouseManager.Razor.AssemblyInfo.cs
+++ WareHouseManager.Razor/obj/Release/net8.0/WareHouseManager.Razor.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WareHouseManager.Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2191b84af889bd52325b22aeee52ce57f15bbe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1935d79cee02f902cfaddcfda5ec80471388dfd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WareHouseManager.Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WareHouseManager.Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
